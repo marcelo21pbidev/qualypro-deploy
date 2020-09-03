7a6250231dc392fd1da003e41503d8ed02a78e3a
Marcelo <marceloeduardo244@gmail.com>
2020-09-03 19:43:44 -0300

    first

---
+++ precache-manifest.9a83acedf51375cc2eb8eafdc530883b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f55de085cf4ec8801c452b8c1290c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "857ee53657393f10ba05",
+    "url": "/static/css/main.06ffda85.chunk.css"
+  },
+  {
+    "revision": "dfe9398907eac84e2776",
+    "url": "/static/js/2.868d093a.chunk.js"
+  },
+  {
+    "revision": "d48bb4deb9bc69d8e49f593901f16876",
+    "url": "/static/js/2.868d093a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "857ee53657393f10ba05",
+    "url": "/static/js/main.7a05e0d5.chunk.js"
+  },
+  {
+    "revision": "7191ed64b9abda1fab29",
+    "url": "/static/js/runtime-main.6192abb3.js"
+  }
+]);
